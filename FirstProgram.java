e77524af08d781378d2b2528d459b9ad310182e8
Abhay singh rajawat <166044966+Abhay2-singh@users.noreply.github.com>
2024-07-27 21:09:42 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+
+public class FirstProgram {
+ public static void main(string[] args) {
+   System.out.print("welcome to kg coding");
+}
+}
+
---
+++ practice/src/question1.java
@@ -0,0 +1,7 @@
+public class question1 {
+    public static void main(String[] args){
+        System.out.println("hello vikash please subcribe my channel");
+        System.out.println("this is my first programe");
+    }
+
+}

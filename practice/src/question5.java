0917f640673a093c5c774c88da7245ccc1b046b2
Abhay singh rajawat <166044966+Abhay2-singh@users.noreply.github.com>
2024-07-31 21:18:59 +0530

    Add files via upload

---
+++ practice/src/question5.java
@@ -0,0 +1,12 @@
+public class question5 {
+    public static void main(String[] args) {
+        int number=10;
+        System.out.println(number);
+        double my_number=3.14;
+        System.out.println(my_number);
+
+        boolean value=true;
+        System.out.println(value);
+
+    }
+}

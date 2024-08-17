59cd77ee8affb565b16215743bb2a87b61aee397
Abhay singh rajawat <166044966+Abhay2-singh@users.noreply.github.com>
2024-08-17 16:31:46 +0530

    Add files via upload

---
+++ Arrays/src/Array.java
@@ -0,0 +1,16 @@
+public class Array {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our programme");
+        int[] Myarr = new int[5];
+        Myarr[0]=2;
+        Myarr[1]=25;
+        Myarr[2]=27;
+        Myarr[3]=28;
+        Myarr[4]=22;
+        System.out.println(Myarr[0]);
+        System.out.println(Myarr[1]);
+        System.out.println(Myarr[2]);
+        System.out.println(Myarr[3]);
+        System.out.println(Myarr[4]);
+    }
+}

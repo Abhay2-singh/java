a75dd7ac75b5f0a76d0e826a7fa59b2d1aa7c5b6
Abhay singh rajawat <166044966+Abhay2-singh@users.noreply.github.com>
2024-09-05 21:12:28 +0530

    Add files via upload

---
+++ KgMath.java
@@ -0,0 +1,9 @@
+public class KgMath {
+    public static void main(String[] args) {
+        System.out.println("welcome to our programme");
+        System.out.println(Math.abs(-99));
+        System.out.println(Math.ceil(5.07));
+        System.out.println(Math.abs(5.07));
+        System.out.println(Math.abs(5.07));
+    }
+}

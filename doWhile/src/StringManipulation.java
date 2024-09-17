7934a85143bddeac69bfe86773e3448369534575
Abhay singh rajawat <166044966+Abhay2-singh@users.noreply.github.com>
2024-09-17 10:21:59 +0530

    Add files via upload

---
+++ doWhile/src/StringManipulation.java
@@ -0,0 +1,11 @@
+public class StringManipulation {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to our programme");
+        String firstName = "vikash";
+        String lastName="rajawat";
+        String fullName=firstName.concat( " ").concat(lastName);
+        System.out.println(fullName.toUpperCase());
+
+    }
+}
